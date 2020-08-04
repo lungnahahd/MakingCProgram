eec64b26d14082fd8c1800122b3cb9502f082e5f
lungnahahd <jyoungjk33@gmail.com>
2020-08-05 01:04:13 +0900

    콘솔창 크기 변경 기능

--- AirplainGame/AirplainGame.cpp
+++ AirplainGame/AirplainGame.cpp
@@ -1,46 +1,10 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include "Evan.h"
 
-//LowHigh 게임을 통한 c언어 연습
-//(컴퓨터가 1~100까지의 숫자를 정하고 이를 플레이어가 맞추는 게임)
-
-//build과정
-//1. 컴퓨터가 임의의 숫자를 선정
-//2. 플레이어가 숫자 입력을 주면 해당 숫자보다 낮으면 "Low", 크면 "High"를 맞추면 "Okay"를 출력
-//3. 숫자를 맞췄을 때, 몇 번의 시도만에 맞추었는지 표시
-
-int main(void) {
-	int num; //컴퓨터의 임의의 숫자
-	int player; //플레이어의 선정 숫자
-	int count = 0; // 몇 번의 시도를 했는지 카운트 해주는 변수
-
-	printf(" < 1 ~ 100 > 까지의 숫자 중 원하는 숫자를 입력해 주세요! \n");
-
-
-	//srand와 rand 함수를 이용해서 임의의 숫자를 받는 과정(범위 : 1 ~ 100)
-	srand(time(NULL));
-	num = rand() % 100 + 1;
-
-	scanf("%d", &player);
-
-	while (player != num)
-	{
-		if (num > player) {
-			printf("정답은 더 커요. 다시 입력해주세요! \n");
-			count = count + 1;
-			scanf("%d", &player);
-		}
-		else if (num < player) {
-			printf("정답은 더 작아요. 다시 입력해주세요! \n");
-			count = count + 1;
-			scanf("%d", &player);
-		}
-	}
-	count = count + 1;
-	printf("정답입니다. 해당 숫자는 %d 이고 시도 횟수는 %d 입니다. \n", num, count);
-
-
+int main(void)
+{
+	SetConsoleSize(30, 30);
 	system("pause");
 	return 0;
 }
---
+++ AirplainGame/Evan.h
@@ -0,0 +1,49 @@
+#include <windows.h>
+#include <stdio.h>
+
+//화면을 지우는 함수
+void Clear(void) 
+{
+	system("cls");
+}
+
+// 커서의 위치를 알려주는 함수
+void GotoXY(int x, int y)
+{
+	COORD pos = { x, y };
+}
+
+// 제목을 변경하는 함수
+void SetTitle(char* _szConsoleName)
+{
+	SetConsoleTitle( _szConsoleName);
+}
+
+// 글자색, 배경색을 변경해주는 함수
+//배경색 0 ~ 15, 글자색 0 ~ 15까지 16색 사용해서 변경하는 함수
+void SetColor(unsigned char _BgColor, unsigned char _TextColor)
+{
+	if (_BgColor > 15 || _TextColor > 15) return;
+	
+	//배경색과 글자색을 동시에 설정하는 함수 - high 4bit:배경색, Low 4 bit : 글자색
+	//0x00f0 이면 f은 흰색 배경 0은 검정 글씨
+	unsigned short ColorNum = (_BgColor << 4) | _TextColor;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ColorNum);
+}
+
+//커서를 안보이게 하는 함수
+void SetCursor(BOOL _bShow)
+{
+	CONSOLE_CURSOR_INFO curInfor;
+	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfor);
+	curInfor.bVisible = _bShow;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfor);
+}
+
+void SetConsoleSize(int _col, int _lines)
+{
+	char setText[100];
+	sprintf_s(setText, "mode con cols=%d lines=%d", _col, _lines);
+	system(setText);
+}
+
