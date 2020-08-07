2db1ffc599816d60a6e132ce3fbf4a3787c708f6
lungnahahd <jyoungjk33@gmail.com>
2020-08-07 17:20:29 +0900

    총알 발사 Ver 2

--- AirplainGame/AirplainGame.cpp
+++ AirplainGame/AirplainGame.cpp
@@ -5,15 +5,19 @@
 int main(void)
 {
 	int x = 15, y = 28;
+	boolean bullet = false;
+	boolean onoff = false;
 	//콘솔창의 크기를 (30,30)으로 조정하는 Evan헤더의 함수
 	SetConsoleSize(30, 30);
+	int bX = 0;
+	int bY = 0;
 
 	while (true)
 	{
 		//별 그림의 잔상을 지워주는 역할 수행
 		Clear();
 
-		//왼,오른쪽 방향키의 입력을 받아서 해당 커서의 위치를 이동시키는 부분, &0x8000을 통해서 꾹 누르던지 한번 누르던지 모두 입력을 받을 수 있다.
+		//입력을 받아서 해당 커서의 위치를 이동시키는 부분, &0x8000을 통해서 꾹 누르던지 한번 누르던지 모두 입력을 받을 수 있다.
 		if (GetAsyncKeyState(VK_LEFT) & 0x8000) { 
 			x--;
 			if (x < 0) {
@@ -27,8 +31,6 @@ int main(void)
 			}
 			
 		}
-
-		
 		if(GetAsyncKeyState(VK_DOWN) & 0x8000) {
 			y++;
 			if (y > 28) {
@@ -39,6 +41,29 @@ int main(void)
 			x++;
 			if (x > 28) {
 				x = 28;
+			}
+		}
+		//총알 발사 Ver 2
+		if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+			if (!bullet) {
+				bX = x;
+				bY = y;
+				bullet = true;
+				onoff = true;
+			}
+			
+			
+
+			
+			
+		}
+		if (onoff) {
+			bY--;
+			GotoXY(bX, bY);
+			printf("º");
+			if (bY < 0) {
+				onoff = false;
+				bullet = false;
 			}
 		}
 
@@ -53,47 +78,47 @@ int main(void)
 		//커서가 while문을 돌면서 너무 빠르게 동작하는 것을 막기 위해 사용되는 코드
 		Sleep(50);
 
-		if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
-			int bulX = x;
-			int bulY = y;
-			while (bulY != 0)
-			{	
-				Clear();
-				bulY--;
-				GotoXY(bulX, bulY);
-				printf("^");
-				if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
-					x--;
-					if (x < 0) {
-						x = 0;
-					}
-				}
-				if (GetAsyncKeyState(VK_UP) & 0x8000) {
-					y--;
-					if (y < 0) {
-						y = 0;
-					}
+		
+		//총알 발사 기능 구현 Ver 1
+	//	if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+	//		int bulX = x;
+	//		int bulY = y;
+	//		while (bulY != 0)
+	//		{	
+	//			Clear();
+	//			bulY--;
+	//			GotoXY(bulX, bulY);
+	//			printf("^");
+	//			if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+	//				x--;
+	//				if (x < 0) {
+	//					x = 0;
+	//				}
+	//			}
+	//			if (GetAsyncKeyState(VK_UP) & 0x8000) {
+	//				y--;
+	//				if (y < 0) {
+	//					y = 0;
+	//				}
 
-				}
-
-
-				if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
-					y++;
-					if (y > 28) {
-						y = 28;
-					}
-				}
-				if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
-					x++;
-					if (x > 28) {
-						x = 28;
-					}
-				}
-				GotoXY(x, y);
-				printf("☆");
-				//Sleep(20);
-			}
-		}
+	//			}
+	//			if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+	//				y++;
+	//				if (y > 28) {
+	//					y = 28;
+	//				}
+	//			}
+	//			if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+	//				x++;
+	//				if (x > 28) {
+	//					x = 28;
+	//				}
+	//			}
+	//			GotoXY(x, y);
+	//			printf("☆");
+	//			//Sleep(20);
+	//		}
+	//	}
 	}
 	
 	system("pause");
