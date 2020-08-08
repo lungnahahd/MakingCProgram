c256ce6c2e914c6a1d52f873e7917c9c9b5010ec
lungnahahd <jyoungjk33@gmail.com>
2020-08-09 01:01:40 +0900

    함수 처리 Ver 2

--- AirplainGame/AirplainGame.cpp
+++ AirplainGame/AirplainGame.cpp
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include "Evan.h"
-
 #define Max 30
 
 //연관성 있는 변수를 함께 관리해 줄 구조체
@@ -18,6 +17,7 @@ OBJECT Enemy[Max];
 OBJECT Player;
 
 int score = 0;
+bool endgame = true;
 
 void makeStar() {
 	for (int i = 0; i < Max; i++) {
@@ -48,6 +48,7 @@ void showStar() {
 int crush() {
 	for (int i = 0; i < Max; i++) {
 		if (Enemy[i].Bcheck && Enemy[i].x == Player.x && Enemy[i].y == Player.y) {
+			endgame = false;
 			GotoXY(Player.x, Player.y);
 			printf("점수 : %d \n", score);
 			system("pause");
@@ -55,14 +56,39 @@ int crush() {
 		}
 	}
 }
+void moveplayer() {
+	// 입력을 받아서 해당 커서의 위치를 이동시키는 부분, & 0x8000을 통해서 꾹 누르던지 한번 누르던지 모두 입력을 받을 수 있다.
+		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+			Player.x--;
+			if (Player.x < 0) {
+				Player.x = 0;
+			}
+		}
+	if (GetAsyncKeyState(VK_UP) & 0x8000) {
+		Player.y--;
+		if (Player.y < 0) {
+			Player.y = 0;
+		}
+
+	}
+	if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+		Player.y++;
+		if (Player.y > 28) {
+			Player.y = 28;
+		}
+	}
+	if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+		Player.x++;
+		if (Player.x > 28) {
+			Player.x = 28;
+		}
+	}
+}
 
 int main(void)
 {
-
-	
-
-	
-	int x = 15, y = 28;
+	Player.x = 15;
+	Player.y = 28;
 	boolean bullet = false;
 	boolean onoff = false;
 	
@@ -74,49 +100,25 @@ int main(void)
 
 	srand(time(NULL));
 
-	while (true)
+	while (endgame)
 	{
-		//별 그림의 잔상을 지워주는 역할 수행
+		//별 그림의 잔상을 지워주는 함수
 		Clear();
-
 		//별을 만드는 함수
 		makeStar();
-
 		//별을 보여주는 함수
 		showStar();
-		
+		//플레이어를 움직이게 해주는 함수
+		moveplayer();
+		//충돌 시 동작 함수
+		crush();
 
-		//입력을 받아서 해당 커서의 위치를 이동시키는 부분, &0x8000을 통해서 꾹 누르던지 한번 누르던지 모두 입력을 받을 수 있다.
-		if (GetAsyncKeyState(VK_LEFT) & 0x8000) { 
-			x--;
-			if (x < 0) {
-				x = 0;
-			}
-		}
-		if (GetAsyncKeyState(VK_UP) & 0x8000) {
-			y--;
-			if (y < 0) {
-				y = 0;
-			}
-			
-		}
-		if(GetAsyncKeyState(VK_DOWN) & 0x8000) {
-			y++;
-			if (y > 28) {
-				y =28;
-			}
-		}
-		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) { 
-			x++;
-			if (x > 28) {
-				x = 28;
-			}
-		}
+		
 		//총알 발사 Ver 2
 		if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
 			if (!bullet) {
-				bX = x;
-				bY = y;
+				bX = Player.x;
+				bY = Player.y;
 				bullet = true;
 				onoff = true;
 			}	
@@ -124,8 +126,7 @@ int main(void)
 		
 
 		
-		//충돌 시 동작 함수
-		crush();
+		
 
 		
 
@@ -146,52 +147,13 @@ int main(void)
 
 
 		//커서의 위치를 변경하고 해당 위치에서 출력 동작 실행
-		GotoXY(x, y);
+		GotoXY(Player.x, Player.y);
 		printf("☆");
 		//커서가 while문을 돌면서 너무 빠르게 동작하는 것을 막기 위해 사용되는 코드
 		Sleep(50);
 
 		
-		//총알 발사 기능 구현 Ver 1
-		/**if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
-			int bulX = x;
-			int bulY = y;
-			while (bulY != 0)
-			{	
-				Clear();
-				bulY--;
-				GotoXY(bulX, bulY);
-				printf("^");
-				if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
-					x--;
-					if (x < 0) {
-						x = 0;
-					}
-				}
-				if (GetAsyncKeyState(VK_UP) & 0x8000) {
-					y--;
-					if (y < 0) {
-						y = 0;
-					}
-
-				}
-				if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
-					y++;
-					if (y > 28) {
-						y = 28;
-					}
-				}
-				if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
-					x++;
-					if (x > 28) {
-						x = 28;
-					}
-				}
-				GotoXY(x, y);
-				printf("☆");
-				//Sleep(20);
-			}
-		}**/
+		
 	}
 	
 	system("pause");
