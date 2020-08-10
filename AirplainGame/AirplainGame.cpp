d621b07d111cbbec9c42dfa6a271725c0bd0d1e4
lungnahahd <jyoungjk33@gmail.com>
2020-08-10 10:32:54 +0900

    비행기 게임 완성 Ver 1

--- AirplainGame/AirplainGame.cpp
+++ AirplainGame/AirplainGame.cpp
@@ -1,8 +1,10 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <conio.h>
 #include "Evan.h"
 #define Max 30
+#define BackColor 15
 
 //연관성 있는 변수를 함께 관리해 줄 구조체
 struct OBJECT
@@ -19,7 +21,18 @@ OBJECT Player;
 int score = 0;
 bool endgame = true;
 
+
+void startSet() {
+	OBJECT begin[Max];
+	for (int i = 0; i < Max; i++) {
+		Enemy[i].Bcheck = begin[i].Bcheck;
+	}
+	
+}
+
+
 void makeStar() {
+	
 	for (int i = 0; i < Max; i++) {
 		if (!Enemy[i].Bcheck) {
 			Enemy[i].x = rand() % 15 * 2;
@@ -36,58 +49,112 @@ void showStar() {
 			GotoXY(Enemy[i].x, Enemy[i].y);
 			printf("*");
 			Enemy[i].y++;
-			if (Enemy[i].y > 28) {
+			if (Enemy[i].y > 28) 
+			{
 				Enemy[i].Bcheck = false;
 				score++;
-
 			}
+			
+		
 		}
 	}
 }
 
-int crush() {
+void crush() {
 	for (int i = 0; i < Max; i++) {
 		if (Enemy[i].Bcheck && Enemy[i].x == Player.x && Enemy[i].y == Player.y) {
 			endgame = false;
 			GotoXY(Player.x, Player.y);
-			printf("점수 : %d \n", score);
-			system("pause");
-			return 0;
+			printf("점수 : %d \n", score);	
+
 		}
 	}
 }
 void moveplayer() {
+	
 	// 입력을 받아서 해당 커서의 위치를 이동시키는 부분, & 0x8000을 통해서 꾹 누르던지 한번 누르던지 모두 입력을 받을 수 있다.
 		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
-			Player.x--;
+			Player.x = Player.x - 2;
 			if (Player.x < 0) {
 				Player.x = 0;
 			}
 		}
 	if (GetAsyncKeyState(VK_UP) & 0x8000) {
-		Player.y--;
+		Player.y = Player.y - 2;
 		if (Player.y < 0) {
 			Player.y = 0;
 		}
 
 	}
 	if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
-		Player.y++;
+		Player.y = Player.y + 2;
 		if (Player.y > 28) {
 			Player.y = 28;
 		}
 	}
 	if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
-		Player.x++;
+		Player.x = Player.x + 2;
 		if (Player.x > 28) {
 			Player.x = 28;
 		}
 	}
 }
 
+void StartMenu() 
+{
+	while (_kbhit())
+	{
+		//입력은 받는데 화면에 보이지는 않는 함수
+		//즉 여기서는 기존 입력을 모두 처리해서 없애주는 역할을 수행
+		_getch();
+	}
+	while (true)
+	{	
+		score = 0;
+		Clear();
+		GotoXY(6, 10);
+		printf("재밌는 비행기 게임");
+		GotoXY(7, 20);
+		printf("Press any Key...");
+		GotoXY(13, 25);
+		printf("Made By LUNGNAHA");
+
+		//아무 버튼이나 누르면 게임을 시작하는 if문
+		if (_kbhit()) {
+			break;
+		}
+		Sleep(50);
+	}
+}
+
+bool ContinueSelect()
+{
+	Clear();
+	bool selectBut = true;
+	while (selectBut) {
+		GotoXY(6, 10);
+		printf("SCORE : %d", score);
+		GotoXY(6, 15);
+		printf("한번 더?");
+		GotoXY(6, 17);
+		printf("Press Y / N");
+		if (GetAsyncKeyState('Y') & 0x8000) {
+			endgame = true;
+			break;
+		}
+		else if (GetAsyncKeyState('N') & 0x8000) {
+			endgame = false;
+			break;
+		}
+		Sleep(50);
+	}
+	return endgame;
+
+}
+
 int main(void)
 {
-	Player.x = 15;
+	Player.x = 14;
 	Player.y = 28;
 	boolean bullet = false;
 	boolean onoff = false;
@@ -96,23 +163,46 @@ int main(void)
 	SetConsoleSize(30, 30);
 	int bX = 0;
 	int bY = 0;
+	//제목을 설정해주는 함수
+	SetTitle("비행기 게임");
+	//배경색을 변경시켜주는 함수
+	SetColor(BackColor, 0);
 
+	StartMenu();
+	Clear();
+	
 
 	srand(time(NULL));
 
 	while (endgame)
 	{
-		//별 그림의 잔상을 지워주는 함수
+		//잔상을 지워주는 함수
 		Clear();
-		//별을 만드는 함수
+		GotoXY(0, 0);
+		printf("SCORE : %d", score);
+		//적기를 만드는 함수
 		makeStar();
-		//별을 보여주는 함수
+		//적기를 보여주는 함수
 		showStar();
 		//플레이어를 움직이게 해주는 함수
 		moveplayer();
 		//충돌 시 동작 함수
 		crush();
-
+		if (!endgame) {
+			ContinueSelect();
+			if (!endgame) {
+				system("pause");
+				return 0;
+			}
+			else if (endgame) {
+				Clear();
+				Sleep(100);
+				startSet();
+				main();
+			}
+		}
+		
+		
 		
 		//총알 발사 Ver 2
 		if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
@@ -123,13 +213,6 @@ int main(void)
 				onoff = true;
 			}	
 		}
-		
-
-		
-		
-
-		
-
 		if (onoff) {
 			bY--;
 			GotoXY(bX, bY);
