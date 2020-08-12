6d29314a92c0697e3854eb6d594289c7d95418bb
lungnahahd <jyoungjk33@gmail.com>
2020-08-12 19:06:28 +0900

    가위바위보 게임 빌드 Ver 1

---
+++ RockPaperScissors/RockPaperScissors.cpp
@@ -0,0 +1,69 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+#include <conio.h>
+
+//구성 : 메인 화면 + 게임 화면 + 결과 화면
+int main(void)
+{
+	printf("가위(1), 바위(2), 보(3) 중에 하나를 선택하세요! \n");
+	int presult = 0;
+	int cresult = 0;
+	
+
+	while (true) {
+		Sleep(50);
+		int player = 0;
+		int com = 0;
+		/*int presult = 0;
+		int cresult = 0;*/
+		srand(time(NULL));
+		com = rand() % 3 + 1;
+		_getch();
+		if (GetAsyncKeyState('1') & 0x8000) {
+			player = 1;
+
+		}
+		else if (GetAsyncKeyState('2') & 0x8000) {
+			player = 2;
+		}
+		else if (GetAsyncKeyState('3') & 0x8000) {
+			player = 3;
+		}
+		else {
+			player = 0;
+		}
+		if (player == 0) {
+			srand(time(NULL));
+			player = rand() % 3 + 1;
+		}
+
+		if (player == com) {
+			printf("비겼습니다. 플레이어 = %d, 컴퓨터 = %d \n", presult, cresult);
+		}
+		else if (player > com) {
+			presult++;
+			printf("이겼습니다. 플레이어 = %d, 컴퓨터 = %d \n", presult, cresult);
+		}
+		else {
+			cresult++;
+			printf("졌습니다. 플레이어 = %d, 컴퓨터 = %d \n", presult, cresult);
+		}
+
+		if (presult == 3) {
+			printf("승리하셨습니다. (%d : %d) \n", presult, cresult);
+			break;
+		}
+		else if (cresult == 3) {
+			printf("패배하셨습니다. (%d : %d) \n", presult, cresult );
+			break;
+		}
+
+
+	}
+
+
+	system("pause");
+	return 0;
+}
